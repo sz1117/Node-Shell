991088aa7a9e8a6195b30ddb347d9e66b1c18391
Simon Zeng <zeng.simonl@gmail.com>
2020-06-19 15:44:27 -0400

    finished command arguments

--- bash.js
+++ bash.js
@@ -1,14 +1,19 @@
-const pwd = require('./pwd')
-const ls = require('./ls')
+const pwd = require('./pwd');
+const ls = require('./ls');
+const cat = require('./cat');
 
 process.stdout.write('prompt >');
 
 process.stdin.on('data', (data) => {
 	const cmd = data.toString().trim();
-	if (cmd === 'pwd'){
-		pwd(cmd)
-	} else if (cmd === 'ls'){
-		ls()
+	if (cmd === 'pwd') {
+		pwd(cmd);
+	}
+	else if (cmd === 'ls') {
+		ls();
+	}
+	else if (cmd) {
+		cat(cmd.slice(4));
 	}
 	process.stdout.write('\nprompt > ');
 });
