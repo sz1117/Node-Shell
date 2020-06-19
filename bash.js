1b1ba0198acd05a51ab2e7c89dded36164a16b3e
Daniel Kim <bungfoon@gmail.com>
2020-06-19 14:44:11 -0400

    adds pwd.js project file for creating a module node-shell section

--- bash.js
+++ bash.js
@@ -1,10 +1,9 @@
+const pwd = require('./pwd')
+
 process.stdout.write('prompt >');
 
 process.stdin.on('data', (data) => {
 	const cmd = data.toString().trim();
-
-	if (cmd === 'pwd') {
-		process.stdout.write(process.cwd());
-	}
+	pwd(cmd)
 	process.stdout.write('\nprompt > ');
 });
---
+++ pwd.js
@@ -0,0 +1,5 @@
+module.exports = function(cmd){
+    if (cmd === 'pwd') {
+		process.stdout.write(process.cwd());
+	}
+}
