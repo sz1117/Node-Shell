79dbd9d19c88ae98632a3b0f3fc97f68dc299f8c
Daniel Kim <bungfoon@gmail.com>
2020-06-19 15:00:21 -0400

    adds ls.js file

--- bash.js
+++ bash.js
@@ -1,9 +1,14 @@
 const pwd = require('./pwd')
+// const ls = require('./ls')
 
 process.stdout.write('prompt >');
 
 process.stdin.on('data', (data) => {
 	const cmd = data.toString().trim();
-	pwd(cmd)
+	if (cmd === 'pwd'){
+		pwd(cmd)
+	} else if (cmd === 'ls'){
+		ls(cmd)
+	}
 	process.stdout.write('\nprompt > ');
 });
--- pwd.js
+++ pwd.js
@@ -1,5 +1,3 @@
 module.exports = function(cmd){
-    if (cmd === 'pwd') {
-		process.stdout.write(process.cwd());
-	}
+	process.stdout.write(process.cwd());
 }
